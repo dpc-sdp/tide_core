b6281eefac2cee764b5483bd95d35a5ec2fcfaa7
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2019-08-22 09:37:15 +1000

    Change the title to scheduled update

--- src/EventSubscriber/TideCoreRouteAlter.php
+++ src/EventSubscriber/TideCoreRouteAlter.php
@@ -20,13 +20,11 @@ class TideCoreRouteAlter extends RouteSubscriberBase {
   protected function alterRoutes(RouteCollection $collection) {
     $route = $collection->get('entity.node.scheduled_transitions');
     if ($route) {
-      $route->setDefault('_title', 'Scheduled publishing');
-      $collection->add('entity.node.scheduled_transitions', $route);
+      $route->setDefault('_title', 'Scheduled updates');
     }
     $route = $collection->get('entity.node.scheduled_transition_add');
     if ($route) {
-      $route->setDefault('_title', 'Add Scheduled publishing');
-      $collection->add('entity.node.scheduled_transition_add', $route);
+      $route->setDefault('_title', 'Add Scheduled update');
     }
   }
 
