d5d4f2d48ac06fb1173000844311369579a410e3
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2019-08-21 16:45:15 +1000

    Delete path alterations.

--- src/EventSubscriber/TideCoreRouteAlter.php
+++ src/EventSubscriber/TideCoreRouteAlter.php
@@ -20,20 +20,13 @@ class TideCoreRouteAlter extends RouteSubscriberBase {
   protected function alterRoutes(RouteCollection $collection) {
     $route = $collection->get('entity.node.scheduled_transitions');
     if ($route) {
-      $route->setPath('/node/{node}/scheduled-publishing');
       $route->setDefault('_title', 'Scheduled publishing');
       $collection->add('entity.node.scheduled_transitions', $route);
     }
     $route = $collection->get('entity.node.scheduled_transition_add');
     if ($route) {
       $route->setDefault('_title', 'Add Scheduled publishing');
-      $route->setPath('/node/{node}/scheduled-publishing/add');
       $collection->add('entity.node.scheduled_transition_add', $route);
-    }
-    $route = $collection->get('entity.scheduled_transition.delete_form');
-    if ($route) {
-      $route->setPath('/admin/scheduled-publishing/{scheduled_transition}/delete');
-      $collection->add('entity.scheduled_transition.delete_form', $route);
     }
   }
 
