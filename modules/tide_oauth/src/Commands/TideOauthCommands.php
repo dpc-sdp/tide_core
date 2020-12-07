df7e777112bffba93f43cade0cd070a556261bbf
MdNadimHossain <getovic@gmail.com>
2020-12-07 18:16:18 +1100

    [tide_oauth_changes] Updated CR changes.

--- modules/tide_oauth/src/Commands/TideOauthCommands.php
+++ modules/tide_oauth/src/Commands/TideOauthCommands.php
@@ -3,6 +3,8 @@
 namespace Drupal\tide_oauth\Commands;
 
 use Drupal\tide_oauth\EnvKeyGenerator;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\simple_oauth\Service\KeyGeneratorService;
 use Drush\Commands\DrushCommands;
 
 /**
@@ -20,14 +22,34 @@ class TideOauthCommands extends DrushCommands {
   protected $envKeyGenerator;
 
   /**
+   * File System.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * Simple Oauth Key Generator Service.
+   *
+   * @var \Drupal\simple_oauth\Service\KeyGeneratorService
+   */
+  protected $keyGenerator;
+
+  /**
    * TideOauthCommands constructor.
    *
    * @param \Drupal\tide_oauth\EnvKeyGenerator $env_key_generator
    *   Env Key Generator.
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *   File system.
+   * @param \Drupal\simple_oauth\Service\KeyGeneratorService $key_generator
+   *   Simple Oauth Key Generator Service.
    */
-  public function __construct(EnvKeyGenerator $env_key_generator) {
+  public function __construct(EnvKeyGenerator $env_key_generator, FileSystemInterface $file_system, KeyGeneratorService $key_generator) {
     parent::__construct();
     $this->envKeyGenerator = $env_key_generator;
+    $this->fileSystem = $file_system;
+    $this->$keyGenerator = $key_generator;
   }
 
   /**
@@ -50,6 +72,16 @@ class TideOauthCommands extends DrushCommands {
       else {
         $this->io()->error('Could not generate OAuth keys from TIDE_OAUTH_*_KEY environment variables.');
       }
+    }
+    else {
+      $this->$keyGenerator->generateKeys('private://');
+      $this->fileSystem->move('private://private.key', EnvKeyGenerator::FILE_PRIVATE_KEY);
+      $this->fileSystem->chmod(EnvKeyGenerator::FILE_PRIVATE_KEY, 0600);
+      $this->fileSystem->move('private://public.key', EnvKeyGenerator::FILE_PUBLIC_KEY);
+      $this->fileSystem->chmod(EnvKeyGenerator::FILE_PUBLIC_KEY, 0600);
+      // Update Simple OAuth settings.
+      $this->envKeyGenerator->setSimpleOauthKeySettings();
+      $this->io()->success('OAuth keys have been created.');
     }
   }
 
