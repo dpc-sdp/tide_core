338421beb2c10a623c9fb317c00572d91cd27795
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2021-08-30 13:25:08 +1000

    fixes lint

--- modules/tide_inactive_users_management/src/Commands/TideInactiveUsersManagementCommands.php
+++ modules/tide_inactive_users_management/src/Commands/TideInactiveUsersManagementCommands.php
@@ -8,19 +8,26 @@ use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\user\Entity\User;
 use Drush\Commands\DrushCommands;
 
+/**
+ * Class TideInactiveUsersManagementCommands.
+ *
+ * @package Drupal\tide_inactive_users_management\Commands
+ */
 class TideInactiveUsersManagementCommands extends DrushCommands {
 
   /**
+   * Block inactive users.
+   *
    * @var \Drupal\Core\Config\ImmutableConfig
    */
-  protected $block_inactive_users;
+  protected $blockInactiveUsers;
 
   /**
    * Idle time.
    *
    * @var int
    */
-  protected $idle_time;
+  protected $idleTime;
 
   /**
    * Block user service.
@@ -34,14 +41,14 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
    *
    * @var bool
    */
-  protected $include_never_accessed;
+  protected $includeNeverAccessed;
 
   /**
    * Exclude users with roles.
    *
    * @var array
    */
-  protected $exclude_user_roles;
+  protected $excludeUserRoles;
 
   /**
    * Logger service.
@@ -57,7 +64,6 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
    */
   protected $config;
 
-
   /**
    * {@inheritdoc}
    */
@@ -66,10 +72,10 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
     $this->config = $configFactory;
     $this->blockUserhandler = $handler;
     $this->logger = $logger->get(InactiveUsersHandler::LOGGER_CHANNEL);
-    $this->block_inactive_users = $this->config->get(InactiveUsersHandler::FORM_SETTINGS_CONFIG_OBJ_NAME);
-    $this->idle_time = $this->block_inactive_users->get('block_inactive_users_idle_time');
-    $this->include_never_accessed = $this->block_inactive_users->get('block_inactive_users_include_never_accessed');
-    $this->exclude_user_roles = $this->block_inactive_users->get('block_inactive_users_exclude_roles');
+    $this->blockInactiveUsers = $this->config->get(InactiveUsersHandler::FORM_SETTINGS_CONFIG_OBJ_NAME);
+    $this->idleTime = $this->blockInactiveUsers->get('block_inactive_users_idle_time');
+    $this->includeNeverAccessed = $this->blockInactiveUsers->get('block_inactive_users_include_never_accessed');
+    $this->excludeUserRoles = $this->blockInactiveUsers->get('block_inactive_users_exclude_roles');
   }
 
   /**
@@ -80,17 +86,19 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
    */
   public function notify() {
     $users = $this->getUsers();
-    foreach ($users as $user) {
-      $last_access = $user->getLastLoginTime();
-      $current_time = time();
-      if ($last_access != 0 && !$user->hasRole('administrator')) {
-        if ($this->blockUserhandler->timestampdiff($last_access, $current_time) >= $this->idle_time) {
-          $this->sendingEmail($user);
+    if ($users) {
+      foreach ($users as $user) {
+        $last_access = $user->getLastLoginTime();
+        $current_time = time();
+        if ($last_access != 0 && !$user->hasRole('administrator')) {
+          if ($this->blockUserhandler->timestampdiff($last_access, $current_time) >= $this->idleTime) {
+            $this->sendingEmail($user);
+          }
         }
-      }
-      if ($this->include_never_accessed == 1 && $last_access == 0) {
-        if ($this->blockUserhandler->timestampdiff($user->getCreatedTime(), $current_time) >= $this->idle_time) {
-          $this->blockUserhandler->sendingEmail($user);
+        if ($this->includeNeverAccessed == 1 && $last_access == 0) {
+          if ($this->blockUserhandler->timestampdiff($user->getCreatedTime(), $current_time) >= $this->idleTime) {
+            $this->sendingEmail($user);
+          }
         }
       }
     }
@@ -104,19 +112,21 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
    */
   public function block() {
     $users = $this->getUsers();
-    foreach ($users as $user) {
-      $last_access = $user->getLastLoginTime();
-      $current_time = time();
-      if ($last_access != 0 && !$user->hasRole('administrator')) {
-        if ($this->blockUserhandler->timestampdiff($last_access, $current_time) >= $this->idle_time + 1) {
-          $user->block();
-          $user->save();
+    if ($users) {
+      foreach ($users as $user) {
+        $last_access = $user->getLastLoginTime();
+        $current_time = time();
+        if ($last_access != 0 && !$user->hasRole('administrator')) {
+          if ($this->blockUserhandler->timestampdiff($last_access, $current_time) >= $this->idleTime + 1) {
+            $user->block();
+            $user->save();
+          }
         }
-      }
-      if ($this->include_never_accessed == 1 && $last_access == 0) {
-        if ($this->blockUserhandler->timestampdiff($user->getCreatedTime(), $current_time) >= $this->idle_time + 1) {
-          $user->block();
-          $user->save();
+        if ($this->includeNeverAccessed == 1 && $last_access == 0) {
+          if ($this->blockUserhandler->timestampdiff($user->getCreatedTime(), $current_time) >= $this->idleTime + 1) {
+            $user->block();
+            $user->save();
+          }
         }
       }
     }
@@ -127,11 +137,14 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
    */
   public function getUsers() {
     $query = \Drupal::entityQuery('user')->condition('status', 1);
-    if (!empty($this->exclude_user_roles)) {
-      $query->condition('roles.target_id', $this->exclude_user_roles, 'NOT IN');
+    if (!empty($this->excludeUserRoles)) {
+      $query->condition('roles.target_id', $this->excludeUserRoles, 'NOT IN');
     }
     $user_ids = $query->execute();
-    return User::loadMultiple($user_ids);
+    if ($user_ids) {
+      return User::loadMultiple($user_ids);
+    }
+    return [];
   }
 
   /**
@@ -141,12 +154,12 @@ class TideInactiveUsersManagementCommands extends DrushCommands {
     $this->logger->info($user->getAccountName() . ' has been notified.');
     $url = \Drupal::request()->getHost();
     if ($sendmail) {
-      $this->blockUserhandler->mailUser($this->block_inactive_users
+      $this->blockUserhandler->mailUser($this->blockInactiveUsers
         ->get('block_inactive_users_from_email'),
         $user->getEmail(), $user->getAccountName(),
-        $this->block_inactive_users
+        $this->blockInactiveUsers
           ->get('block_inactive_users_email_subject'),
-        $this->block_inactive_users
+        $this->blockInactiveUsers
           ->get('block_inactive_users_email_content'),
         $url);
     }
