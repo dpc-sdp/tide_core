7a0f169272375062799e3511b7e5f7086b38d93f
Md Nadim Hossain <getovic@gmail.com>
2023-05-30 16:57:01 +1000

    [SRM-870] Added changes for d10.

--- dev-tools.sh
+++ dev-tools.sh
@@ -34,5 +34,6 @@
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
 # export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=30ecab0f7ed830e47de7cf71201b9fd0f3d33fdd
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
