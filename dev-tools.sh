b885e6dae1f788fac287b1e301986ecd1fd41b9c
Md Nadim Hossain <getovic@gmail.com>
2023-06-07 09:59:13 +1000

    Added new plugin to be allowed in dev-tools composer.

--- dev-tools.sh
+++ dev-tools.sh
@@ -34,6 +34,6 @@
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
 # export GH_COMMIT=COMMIT_SHA
-export GH_COMMIT=234859a294da1a70d4b77734f67b9881287690a8
+export GH_COMMIT=f058d03db0ec6a498483f08e27b72c416e89a2ea
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
