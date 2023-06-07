d6aeec9d1456f9fdef57bf6b3fba2b17fe4209f0
Md Nadim Hossain <getovic@gmail.com>
2023-06-07 13:30:59 +1000

    Updated dev-tools

--- dev-tools.sh
+++ dev-tools.sh
@@ -34,6 +34,6 @@
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
 # export GH_COMMIT=COMMIT_SHA
-export GH_COMMIT=f058d03db0ec6a498483f08e27b72c416e89a2ea
+export GH_COMMIT=a4b884db1479c19a102215fcb9a06296b30626ac
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
