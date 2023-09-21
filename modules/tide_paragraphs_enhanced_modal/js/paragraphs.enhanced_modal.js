8ea54b4077a44607964f5fe3df2dc1ca9b51b98f
Md Nadim Hossain <getovic@gmail.com>
2023-09-21 10:36:05 +1000

    [SRM-870] Updated with latest drupal core.

--- modules/tide_paragraphs_enhanced_modal/js/paragraphs.enhanced_modal.js
+++ modules/tide_paragraphs_enhanced_modal/js/paragraphs.enhanced_modal.js
@@ -9,10 +9,8 @@
 
   Drupal.behaviors.paragraphsEnhancedModalAdd = {
     attach: function (context) {
-      console.log('I am here');
       $(once('add-click-handler', '.paragraphs-add-dialog-enhanced .paragraphs-add-dialog-row', context)).on('click', function (event) {
         var $button = $(this).find('input.button').first();
-        console.log($button);
         $button.trigger('mousedown');
         // Stop default execution of click event.
         event.preventDefault();
