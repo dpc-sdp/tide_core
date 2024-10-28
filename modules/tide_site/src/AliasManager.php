aca3f04cf3513c5039f4be7589fc96aa2195c690
Md Nadim Hossain <getovic@gmail.com>
2024-10-28 13:41:38 +1100

    Made the time required as it is marked as deprecated for Drupal 10.3.

--- modules/tide_site/src/AliasManager.php
+++ modules/tide_site/src/AliasManager.php
@@ -26,10 +26,9 @@ class AliasManager extends CoreAliasManager {
   /**
    * {@inheritdoc}
    */
-  public function __construct(AliasRepositoryInterface $repository, AliasWhitelistInterface $whitelist, LanguageManagerInterface $language_manager, CacheBackendInterface $cache, protected ?TimeInterface $time = NULL)
-  {
+  public function __construct(AliasRepositoryInterface $repository, AliasWhitelistInterface $whitelist, LanguageManagerInterface $language_manager, CacheBackendInterface $cache, protected ?TimeInterface $time, AliasStorageHelper $alias_helper) {
     parent::__construct($repository, $whitelist, $language_manager, $cache, $time);
-    $this->aliasHelper = \Drupal::service('tide_site.alias_storage_helper');
+    $this->aliasHelper = $alias_helper;
   }
 
   /**
