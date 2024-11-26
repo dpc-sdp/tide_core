52194500c931759c4ec1e1497187cecc3cf2dd9a
Yeni Atencio <yeni.atencio@dpc.vic.gov.au>
2024-11-26 13:12:37 +1100

    testing js

---
+++ modules/tide_landing_page/js/hero_image_theme.js
@@ -0,0 +1,38 @@
+(function (Drupal) {
+  "use strict";
+
+  Drupal.behaviors.heroImageTheme = {
+    attach: function (context, settings) {
+      const imageTheme = context.querySelector(
+        'select[data-drupal-selector^="edit-field-landing-page-hero-theme"]'
+      );
+
+      if (imageTheme !== null) {
+        setImageTheme();
+
+        const headerStyles = context.querySelectorAll(
+          'input[data-drupal-selector^="edit-header-style-options"]'
+        );
+
+        if (headerStyles.length > 0) {
+          headerStyles.forEach((style) => {
+            style.addEventListener("change", setImageTheme);
+          });
+        }
+      }
+
+      function setImageTheme() {
+        const selectedHeaderStyle = document.querySelector(
+          'input[data-drupal-selector^="edit-header-style-options"]:checked'
+        );
+
+        if (!selectedHeaderStyle) {
+          return; // Exit if no style option is selected.
+        }
+
+        let defaultHeaderStyle = selectedHeaderStyle.value;
+        imageTheme.disabled = defaultHeaderStyle === "corner" ? false : true;
+      }
+    },
+  };
+})(Drupal);
