a1da50bc6817f7ed68bd71be5079868d6fe9d033
Yeni Atencio <yeni.atencio@dpc.vic.gov.au>
2025-02-23 15:57:50 +1100

    test

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=d8c90e45c6a8438976f4d25a1e8bdb01795a2a29
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
