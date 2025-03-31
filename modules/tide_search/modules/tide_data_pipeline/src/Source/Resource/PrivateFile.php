eab93f2345cd845c45c3b44e8438610306108ee0
sharmasahil <sahil.sharma@dgs.vic.gov.au>
2025-04-01 09:54:53 +1100

    Private dataset file upload and process

--- modules/tide_search/modules/tide_data_pipeline/src/Source/Resource/PrivateFile.php
+++ modules/tide_search/modules/tide_data_pipeline/src/Source/Resource/PrivateFile.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Drupal\tide_data_pipeline\Source\FileResource;
+namespace Drupal\tide_data_pipeline\Source\Resource;
 
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
@@ -11,47 +11,34 @@ use Drupal\data_pipelines\Source\Resource\SourceResourceBase;
 use Drupal\file\FileInterface;
 
 /**
- * A class for the Privatefile.
+ * A class for the file resource.
  */
 final class PrivateFile extends SourceResourceBase {
 
   /**
-   * {@inheritDoc}
+   * {@inheritdoc}
    */
-  public function getResource(DatasetInterface $dataset, string $field_name) {
+  public function getResource(DatasetInterface $dataset, string $field_name): mixed {
     if (!empty($file = self::getFieldValue($dataset, $field_name, 'entity'))) {
       assert($file instanceof FileInterface);
-
-      // Check if the file is stored in the private file system.
-      if ($file->getFileUri()) {
-        // Ensure the file is part of the private files folder.
-        $file_uri = $file->getFileUri();
-        if (strpos($file_uri, 'private://') === 0) {
-          return fopen($file_uri, 'r');
-        }
-      }
+      return fopen($file->getFileUri(), 'r');
     }
     return NULL;
   }
 
   /**
-   * {@inheritDoc}
+   * {@inheritdoc}
    */
   public function getResourceBaseFieldDefinition(array $source_plugin_definition): BaseFieldDefinition {
-    // Create the field definition for the file.
-    $field_definition = BaseFieldDefinition::create('file')
-      ->setLabel(new TranslatableMarkup('Private File'))
+    return BaseFieldDefinition::create('file')
+      ->setLabel(new TranslatableMarkup('File'))
       ->setDescription(new TranslatableMarkup('The file for the dataset.'))
       ->setSetting('file_extensions', $source_plugin_definition['id'])
       ->setRequired(TRUE)
+      ->setSetting('uri_scheme', 'private')
       ->setDisplayOptions('form', [
         'type' => 'file_generic',
       ]);
-
-    // Set the upload location to the private directory.
-    $field_definition->setSetting('file_directory', 'private://');
-
-    return $field_definition;
   }
 
 }
