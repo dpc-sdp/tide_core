6831b362c67ce61115839b93d92b9bb0da35f7e5
Md Nadim Hossain <getovic@gmail.com>
2025-08-18 12:48:07 +1000

    Submitted the number of shards from form.

--- modules/tide_search/src/Plugin/search_api/backend/TideElasticsearchBackend.php
+++ modules/tide_search/src/Plugin/search_api/backend/TideElasticsearchBackend.php
@@ -43,6 +43,35 @@ class TideElasticsearchBackend extends SearchApiElasticsearchBackend {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+
+    // Handle synonyms as before.
+    $values['advanced']['synonyms'] = explode(\PHP_EOL, $form_state->getValue([
+      'advanced',
+      'synonyms',
+    ], ''));
+
+    // Save full configuration.
+    $this->setConfiguration($values);
+
+    // Save connector config.
+    $this->configuration['connector'] = $form_state->getValue('connector');
+    $connector = $this->getConnector();
+    if ($connector instanceof PluginFormInterface) {
+      $connector_form_state = SubformState::createForSubform($form['connector_config'], $form, $form_state);
+      $connector->submitConfigurationForm($form['connector_config'], $connector_form_state);
+      // Overwrite the form values with type casted values.
+      $form_state->setValue('connector_config', $connector->getConfiguration());
+    }
+
+    // 👇 Save your custom field explicitly.
+    $this->configuration['number_of_shards'] = (int) $form_state->getValue('number_of_shards');
+  }
+
+  /**
    * Get the configured number of shards.
    *
    * @return int
