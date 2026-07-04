fd30653c72fe78f4e0283d7dab709d99ff579231
Vincent Gao <vincent.gao@dgs.vic.gov.au>
2026-07-04 18:12:43 +1000

    Removed legacy Ahoy/Lagoon/Bay development tooling.
    Replaced by DDEV (see previous commit). CircleCI scripts still reference
    ahoy and need a follow-up rewrite.

--- .docker/Dockerfile.php
+++
@@ -1,8 +0,0 @@
-# @see https://github.com/dpc-sdp/bay/blob/master/bay/images/Dockerfile.php
-ARG BAY_IMAGE_VERSION
-ARG CLI_IMAGE
-FROM ${CLI_IMAGE:-cli} as cli
-
-FROM ghcr.io/dpc-sdp/bay/php-fpm:${BAY_IMAGE_VERSION}
-
-COPY --from=cli /app /app
