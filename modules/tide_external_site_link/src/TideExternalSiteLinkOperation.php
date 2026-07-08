ff4ce40cc8cb72ae3b851431a0d278e7bed862eb
David Featherston <david.featherstone@dpc.vic.gov.au>
2026-07-08 11:58:29 +1000

    [SD-1738] add external site link content type (#779)
    * feat: add external site link content type
    
    * fix: remove tide_site_restriction from for display conf in tide_external_site_link
    
    * feat: add content category to external site link, update keyword type to string
    
    * feat: switch to select2 for content category
    
    * feat: disable share and preview links for external_site_links
    
    * feat: remove sections for external site link node form, update status message and node path
    
    * fix: appease the linter
    
    * feat: remove preview button from external site link node form and add more tests
    
    * feat: add extra test to check external site link path
    
    * chore: fix content type reference
    
    * chore: remove tide_external_site_link.permissions.yml file
    
    * feat: add site restriction support to external site link
    
    * feat: add featured image help text, update external site link permissions
    
    * fix: external site link role permission
    
    * chore: add approver plus permissions
    
    * chore: more permissions updates, add core install hook
    
    * Moved administrator-role-dependent tide_site views to optional config
    
    views.view.media_report and views.view.tide_content_report depend on
    user.role.administrator, which tide_core only provides as optional config.
    During a site installation with the tide profile, optional config is
    deferred to the end of the install, so tide_site's install-time config
    failed validation with an UnmetDependenciesException. As optional config,
    the views are imported at the end of installation, after the role exists.
    
    ---------
    
    Co-authored-by: vincent-gao <vincent.gao@dpc.vic.gov.au>
    Co-authored-by: Vincent Gao <vincent.gao@dgs.vic.gov.au>

---
+++ modules/tide_external_site_link/src/TideExternalSiteLinkOperation.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Drupal\tide_external_site_link;
+
+use Drupal\search_api\Item\Field;
+use Drupal\user\Entity\Role;
+use Drupal\workflows\Entity\Workflow;
+
+/**
+ * Helper class for install/update ops.
+ */
+class TideExternalSiteLinkOperation {
+
+  /**
+   * Add to editorial workflows.
+   */
+  public static function addToWorkflows() {
+    if (!(\Drupal::moduleHandler()->moduleExists('workflows'))) {
+      return;
+    }
+
+    $editorial_workflow = Workflow::load('editorial');
+
+    if ($editorial_workflow) {
+      $editorial_workflow->getTypePlugin()->addEntityTypeAndBundle('node', 'external_site_link');
+      $editorial_workflow->save();
+    }
+  }
+
+  /**
+   * Add external site link content type scheduled transitions.
+   */
+  public static function addToScheduledTransitions() {
+    if (!(\Drupal::moduleHandler()->moduleExists('scheduled_transitions'))) {
+      return;
+    }
+    $config_factory = \Drupal::configFactory();
+    $config = $config_factory->getEditable('scheduled_transitions.settings');
+    $bundles = $config->get('bundles');
+    if ($bundles) {
+      $enabled_bundles = array_column($bundles, 'bundle');
+      if (!in_array('external_site_link', $enabled_bundles)) {
+        $bundles[] = ['entity_type' => 'node', 'bundle' => 'external_site_link'];
+        $config->set('bundles', $bundles)->save();
+      }
+    }
+    else {
+      $bundles[] = ['entity_type' => 'node', 'bundle' => 'external_site_link'];
+      $config->set('bundles', $bundles)->save();
+    }
+  }
+
+  /**
+   * Assign permissions.
+   */
+  public static function assignNecessaryPermissions() {
+    $role_permissions = [
+      'editor' => [
+        'clone external_site_link content',
+        'create external_site_link content',
+        'edit any external_site_link content',
+        'edit own external_site_link content',
+        'revert external_site_link revisions',
+        'view external_site_link revisions',
+        'view scheduled transitions node external_site_link',
+      ],
+      'site_admin' => [
+        'add scheduled transitions node external_site_link',
+        'clone external_site_link content',
+        'create external_site_link content',
+        'delete any external_site_link content',
+        'delete external_site_link revisions',
+        'delete own external_site_link content',
+        'edit any external_site_link content',
+        'edit own external_site_link content',
+        'reschedule scheduled transitions node external_site_link',
+        'revert external_site_link revisions',
+        'view external_site_link revisions',
+        'view scheduled transitions node external_site_link',
+      ],
+      'approver' => [
+        'add scheduled transitions node external_site_link',
+        'clone external_site_link content',
+        'create external_site_link content',
+        'edit any external_site_link content',
+        'edit own external_site_link content',
+        'reschedule scheduled transitions node external_site_link',
+        'revert external_site_link revisions',
+        'view external_site_link revisions',
+        'view scheduled transitions node external_site_link',
+      ],
+      'approver_plus' => [
+        'add scheduled transitions node external_site_link',
+        'clone external_site_link content',
+        'create external_site_link content',
+        'edit any external_site_link content',
+        'edit own external_site_link content',
+        'reschedule scheduled transitions node external_site_link',
+        'revert external_site_link revisions',
+        'view external_site_link revisions',
+        'view scheduled transitions node external_site_link',
+      ],
+      'contributor' => [
+        'clone external_site_link content',
+        'create external_site_link content',
+        'edit any external_site_link content',
+        'edit own external_site_link content',
+        'revert external_site_link revisions',
+        'view external_site_link revisions',
+        'view scheduled transitions node external_site_link',
+      ],
+    ];
+
+    foreach ($role_permissions as $role => $permissions) {
+      if (Role::load($role) && !is_null(Role::load($role))) {
+        user_role_grant_permissions(Role::load($role)->id(), $permissions);
+      }
+    }
+  }
+
+  /**
+   * Add fields to search API.
+   */
+  public static function addFieldsToSearchApi() {
+    $moduleHandler = \Drupal::service('module_handler');
+
+    if (!$moduleHandler->moduleExists('tide_search')) {
+      return;
+    }
+
+    $index_storage = \Drupal::entityTypeManager()->getStorage('search_api_index');
+    $index = $index_storage->load('node');
+
+    // Index the keywords field.
+    $keywords_field = new Field($index, 'field_content_keywords');
+    $keywords_field->setType('text');
+    $keywords_field->setPropertyPath('field_content_keywords');
+    $keywords_field->setDatasourceId('entity:node');
+    $keywords_field->setLabel('Content keywords');
+    $index->addField($keywords_field);
+
+    $index->save();
+  }
+
+  /**
+   * Add site restriction support to site and primary site fields.
+   */
+  public static function addSiteRestrictionSupport() {
+    if (!(\Drupal::moduleHandler()->moduleExists('tide_site_restriction'))) {
+      return;
+    }
+
+    $config = \Drupal::configFactory()->getEditable('core.entity_form_display.node.external_site_link.default');
+    $config->set('content.field_node_primary_site.type', 'tide_site_restriction_field_widget');
+    $config->set('content.field_node_site.type', 'tide_site_restriction_field_widget');
+    $config->save();
+  }
+
+}
